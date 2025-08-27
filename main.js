3af4696d8c5da8efccc0727cd174323541f79496
felipadas07 <felipe.santos.silva22@escola.pr.gov.br>
2025-08-27 12:26:09 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
-// cria referência ao form e aos elementos h3 e h4 (resposta)
-const frm = document.querySelector("form")
-const resp1 = document.querySelector("h3")
-const resp2 = document.querySelector("h4")
+// cria referência do form e aos elementos de resposta (pelo seu id)
+const frm = document.querySelector("form");
+const resp1 = document.querySelector("#outResp1");
+const resp2 = document.querySelector("#outResp2");
+const resp3 = document.querySelector("#outResp3");
 
-// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+// cria um "ouvinte" de evento, acionando quando o botão submit for clicado
 frm.addEventListener("submit", (e) => {
-    const titulo = frm.inTitulo.value  // obtém conteúdo dos campos
-    const duracao = Number(frm.inDuracao.value)
+    const veiculo = frm.inVeiculo.value; // obtém o conteúdo dos campos
+    const preco = Number(frm.inPreco.value);
 
-    const horas = Math.floor(duracao / 60)  // arredonda para baixo resultado
-    const minutos = duracao % 60  // obtém o resto da divisão
+    const entrada = preco * 0.50; // calcula valor da entrada
+    const parcela = (preco * 0.50) / 12; // e das parcelas
 
-    resp1.innerText = titulo  // exibe as respostas
-    resp2.innerText = `${horas} hora(s) e ${minutos} minuto(s)`
+    resp1.innerText = `Promoção: ${veiculo}`; // exibe as respostas
+    resp2.innerText = `Entrada de R$: ${entrada.toFixed(2)}`;
+    resp3.innerText = `+12x de R$ ${parcela.toFixed(2)}`;
 
-    e.preventDefault()  // evita envio do form
-})
+    e.preventDefault(); // evita envio do form
+});
